01d8b405f08d5c8a374e864349b035563832a6ad
Andika Bahari <andikabahari48@gmail.com>
2023-09-25 15:11:38 +0700

    all tests

--- test_stage.sh
+++ test_stage.sh
@@ -7,6 +7,36 @@ case $1 in
   '2') 
     CODECRAFTERS_CURRENT_STAGE_SLUG='match_digit'
     ;;
+  '3') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='match_alphanumeric'
+    ;;
+  '4') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='positive_character_groups'
+    ;;
+  '5') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='negative_character_groups'
+    ;;
+  '6') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='combining_character_classes'
+    ;;
+  '7') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='start_of_string_anchor'
+    ;;
+  '8') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='end_of_string_anchor'
+    ;;
+  '9') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='one_or_more_quantifier'
+    ;;
+  '10') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='zero_or_one_quantifier'
+    ;;
+  '11') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='wildcard'
+    ;;
+  '12') 
+    CODECRAFTERS_CURRENT_STAGE_SLUG='alternation'
+    ;;
   *)
     echo 'Invalid stage'
     exit
